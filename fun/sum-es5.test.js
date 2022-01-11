28d5fdadfe04747db1d36ec100c690333cdd01f8
Anil Kumar <anil.kumar@peopletech.com>
2022-01-11 14:06:55 +0530

    Fix: remove import issues

---
+++ fun/sum-es5.test.js
@@ -0,0 +1,7 @@
+const sum = require("./sum-es5");
+
+describe('FUNCTION TESTING',() => {
+  test("sum testing", () => {
+    expect(sum(2, 3)).toBe(5);
+  });
+});
--- fun/sum.test.js
+++
@@ -1,8 +0,0 @@
-const sum = require("./sum");
-const { describe, test } = require("jest");
-
-// describe('FUNCTION TESTING',() => {
-test("sum testing", () => {
-  expect(sum(2, 3)).toBe(5);
-});
-// });
